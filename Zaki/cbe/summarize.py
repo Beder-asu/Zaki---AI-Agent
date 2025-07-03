baacf481fa77062175ce52d286a6214d708fcdcf
Mohamed Ahmed Beder Mohamed <mobeder88@gmail.com>
2025-07-03 07:38:45 +0300

    Update summarize.py

--- Zaki/cbe/summarize.py
+++ Zaki/cbe/summarize.py
@@ -4,7 +4,7 @@ import io
 
 API_URL = "https://router.huggingface.co/featherless-ai/v1/chat/completions"
 HEADERS = {
-    "Authorization": "Bearer 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",
+    "Authorization": "Bearer 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",
 }
 
 # Next step: configure gmail automationto run the app solely atonomus
